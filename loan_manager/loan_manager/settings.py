2de3457ef89a6a97b044a41683f9325a504c1330
Filipe Barcelos <filipekn4@gmail.com>
2025-02-10 21:25:08 -0300

    Changes env file reading on settings

--- loan_manager/loan_manager/settings.py
+++ loan_manager/loan_manager/settings.py
@@ -11,19 +11,20 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import Config, RepositoryEnv
+# from decouple import Config, RepositoryEnv
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Take environment variables from .env file
-env_config = Config(RepositoryEnv(os.path.join(BASE_DIR, '.env')))
+# Take environment variables from .env file (Unnecessary I think)
+# env_config = Config(RepositoryEnv(os.path.join(BASE_DIR, '.env')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&z$fuxt(_pp*dzzp!0!bd_-*ruwj=y4o*1t&i#1($!(4n5tne)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,11 +95,11 @@ WSGI_APPLICATION = 'loan_manager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env_config('DB_NAME', default='loan_manager_db'),
-        'USER': env_config('DB_USER', default='postgres'),
-        'PASSWORD': env_config('DB_PASSWORD', default='postgres'),
-        'HOST': env_config('DB_HOST', default='localhost'),
-        'PORT': env_config('DB_PORT', default='5432'),
+        'NAME': config('DB_NAME', default='loan_manager_db'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='postgres'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
